e2bdf63fa8b481b91d7e56fac5c4870641d42a73
spacemapache <142768034+spacemapache@users.noreply.github.com>
2024-05-16 13:08:48 -0700

    added firebase database to js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://playground-d9762-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingList = ref(database, "shoppingList");
+
 const inputElement = document.getElementById("inputField");
+const addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", addToCart);
 
 function addToCart() {
   let inputValue = inputElement.value;
+  push(shoppingList, inputValue);
   console.log(inputValue);
 }
